4a31670026f77d44097d099743bd7e22a0567a71
marciliodevjava <marciliodevjava@gmail.com>
2024-03-14 18:48:09 -0300

    Create variaveis_do_sistema

---
+++ variaveis/variaveis_do_sistema.py
@@ -0,0 +1,23 @@
+import datetime
+
+BASE_PATH_HTTP = '/pro'
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}:{port}/{database}'.format(
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='1234567890',
+        servidor='127.0.0.1',
+        port='3306',
+        database='biblioteca'
+    )
+
+SECRET = 'JWT_SECRET_KEY'
+SECRET_KEY = '123'
+DATABASE_URI = 'SQLALCHEMY_DATABASE_URI'
+HEADER_NAME = 'JWT_HEADER_NAME'
+HEADER_TYPE = 'JWT_HEADER_TYPE'
+NM_ID_SESSAO = "nmIdSessao"
+TOKEN = 'JWT_ACCESS_TOKEN_EXPIRES'
+TIME_TOKEN = datetime.timedelta(minutes=60)
+NONE = None
