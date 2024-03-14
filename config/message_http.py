9d902cf8615dcad319bb6b5a3fe1c442721881eb
marciliodevjava <marciliodevjava@gmail.com>
2024-03-14 18:47:39 -0300

    Create message_http

---
+++ config/message_http.py
@@ -0,0 +1,24 @@
+from flask import jsonify
+
+from enumerate.message import ErroServidorMessage, MessageToken
+
+
+class ResponseErro:
+
+    @classmethod
+    def token_expirado(cls):
+        return jsonify(
+            {
+                'error': MessageToken.TOKEN_EXPIRADO.value,
+                'message': MessageToken.TOKEN_LOGIN_NOVAMENTE.value
+            }
+        )
+
+    @classmethod
+    def http_exception(cls):
+        return jsonify(
+            {
+                'error': ErroServidorMessage.ERRO_INTERNO_SERVIDOR.value,
+                'message': ErroServidorMessage.ERRO_OCORREU_ERRO_INTERNO_SERVIDOR.value
+            }
+        )
