7a7d409725fa2975587293c222973b562939ac60
marciliodevjava <marciliodevjava@gmail.com>
2024-03-15 23:03:10 -0300

    Update usuario_service

---
+++ service/usuario_service.py
@@ -0,0 +1,17 @@
+class UsuarioService:
+
+    @classmethod
+    def cadastrar_usuario(cls, usuario):
+        pass
+
+    @classmethod
+    def buscar_usuario(cls, id):
+        pass
+
+    @classmethod
+    def atualizar_usuario(cls, id, usuario):
+        pass
+
+    @classmethod
+    def deletar_usuario(cls, id):
+        pass
