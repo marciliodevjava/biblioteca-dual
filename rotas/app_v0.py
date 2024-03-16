e7c399f66e64039d93a4879cb8b2ed5bd06001ad
marciliodevjava <marciliodevjava@gmail.com>
2024-03-15 22:46:00 -0300

    Create app_V0

--- rotas/app_v0.py
+++ rotas/app_v0.py
@@ -1,4 +1,12 @@
 from flask_restful import Api
+
+from resource.usuario_resource import UsuarioResource
+
+
 def config_routes_v0(api: Api):
+    api.add_resource(UsuarioResource, '/cadastro', methods=['POST'], endpoint='cadastro_usuario')
+    api.add_resource(UsuarioResource, '/cadastro', methods=['GET'], endpoint='buscar_usuario')
+    api.add_resource(UsuarioResource, '/cadastro', methods=['PUT'], endpoint='atualizar_usuario')
+    api.add_resource(UsuarioResource, '/cadastro', methods=['DELETE'], endpoint='deletar_usuario')
 
     return api
