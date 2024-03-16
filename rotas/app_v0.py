0b879b1f680ad9fb1eb1cb682236416f61e22d45
marciliodevjava <marciliodevjava@gmail.com>
2024-03-15 22:50:46 -0300

    Update app_V0

--- rotas/app_v0.py
+++ rotas/app_v0.py
@@ -1,12 +1,13 @@
 from flask_restful import Api
 
 from resource.usuario_resource import UsuarioResource
+from variaveis.variaveis_do_sistema import BASE_PATH_HTTP
 
 
 def config_routes_v0(api: Api):
-    api.add_resource(UsuarioResource, '/cadastro', methods=['POST'], endpoint='cadastro_usuario')
-    api.add_resource(UsuarioResource, '/cadastro', methods=['GET'], endpoint='buscar_usuario')
-    api.add_resource(UsuarioResource, '/cadastro', methods=['PUT'], endpoint='atualizar_usuario')
-    api.add_resource(UsuarioResource, '/cadastro', methods=['DELETE'], endpoint='deletar_usuario')
+    api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro', methods=['POST'], endpoint='cadastro_usuario')
+    api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro/<int:id>', methods=['GET'], endpoint='buscar_usuario')
+    api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro/<int:id>', methods=['PUT'], endpoint='atualizar_usuario')
+    api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro/<int:id>', methods=['DELETE'], endpoint='deletar_usuario')
 
     return api
