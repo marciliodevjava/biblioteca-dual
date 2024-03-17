25d5620b966f58f5982dbe2e36b7be02bcd7094e
marciliodevjava <marciliodevjava@gmail.com>
2024-03-17 16:12:54 -0300

    Update message

--- enumerate/message.py
+++ enumerate/message.py
@@ -1,5 +1,7 @@
 from enum import Enum
+
 from variaveis.variaveis_enum import TOKEN, LOGIN
+
 
 class MessageToken(Enum):
     TOKEN_LOGIN_NOVAMENTE = f'Faça {LOGIN} novamente para obter um novo {TOKEN}'
@@ -9,3 +11,11 @@ class MessageToken(Enum):
 class ErroServidorMessage(Enum):
     ERRO_OCORREU_ERRO_INTERNO_SERVIDOR = 'Ocorreu um erro interno no servidor.'
     ERRO_INTERNO_SERVIDOR = 'Erro interno no servidor'
+
+
+class UsuarioMessage(Enum):
+    USUARIO_CADASTRO_COM_SUCESSO = 'Usuario cadastrado com sucesso.'
+
+
+class UsuarioFormulario(Enum):
+    O_CAMPO_NOME = 'O campo nome não foi enviado.'
