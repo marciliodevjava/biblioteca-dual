5c4f13d103ecf1990f32e3895299e18caa14eb47
marciliodevjava <marciliodevjava@gmail.com>
2024-03-17 16:28:47 -0300

    Update encripty_pasword

--- utils/encripty_pasword.py
+++ utils/encripty_pasword.py
@@ -1,9 +1,11 @@
 import bcrypt
 
 
-def gerador_senha(senha):
-    return bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+class Senha:
+    @classmethod
+    def gerador_senha(senha):
+        return bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
 
-
-def check_senha(usuario_senha, senha_banco):
-    return bcrypt.checkpw(usuario_senha.encode('utf-8'), senha_banco.encode('utf-8'))
+    @classmethod
+    def check_senha(usuario_senha, senha_banco):
+        return bcrypt.checkpw(usuario_senha.encode('utf-8'), senha_banco.encode('utf-8'))
