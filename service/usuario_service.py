dcc7dc20751964930c1ac39d20e0b6c5e97f5422
marciliodevjava <marciliodevjava@gmail.com>
2024-03-17 16:21:44 -0300

    Update usuario_service

--- service/usuario_service.py
+++ service/usuario_service.py
@@ -2,7 +2,13 @@ class UsuarioService:
 
     @classmethod
     def cadastrar_usuario(cls, usuario):
-        pass
+        nome = usuario['nome']
+        email = usuario['email']
+        genero = usuario['genero']
+        ddi = usuario['ddi']
+        ddd = usuario['ddd']
+        telefone = usuario['telefone']
+        senha = usuario['senha']
 
     @classmethod
     def buscar_usuario(cls, id):
