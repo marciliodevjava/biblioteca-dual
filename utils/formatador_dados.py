af8361ec2532b37a130e178f1bb6e1524669f2a7
marciliodevjava <marciliodevjava@gmail.com>
2024-03-17 16:28:22 -0300

    Update formatador_dados

---
+++ utils/formatador_dados.py
@@ -0,0 +1,25 @@
+class FormatadorDados:
+
+    @classmethod
+    def formatador_nome(cls, nome):
+        pass
+
+    @classmethod
+    def formatador_email(cls, email):
+        pass
+
+    @classmethod
+    def formatador_genero(cls, genero):
+        pass
+
+    @classmethod
+    def formatador_ddi(cls, ddi):
+        pass
+
+    @classmethod
+    def formatador_ddd(cls, ddd):
+        pass
+
+    @classmethod
+    def formatador_telefone(cls, telefone):
+        pass
