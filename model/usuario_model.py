74a1b03256b7b535578513e20b22e788f93350d2
marciliodevjava <marciliodevjava@gmail.com>
2024-03-17 22:10:31 -0300

    Update usuario_model

--- model/usuario_model.py
+++ model/usuario_model.py
@@ -6,10 +6,10 @@ class UsuarioModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150), nullable=False)
     email = db.Column(db.String(150), nullable=False)
-    genero = db.Column(db.Collumn(10), nullable=False)
-    ddi = db.Column(db.Integer(4), nullable=False)
-    ddd = db.Column(db.Integer(4), nullable=False)
-    telefone = db.Column(db.Integer(9), nullable=False)
+    genero = db.Column(db.String(10), nullable=False)
+    ddi = db.Column(db.Integer(), nullable=False)
+    ddd = db.Column(db.Integer(), nullable=False)
+    telefone = db.Column(db.Integer(), nullable=False)
     senha = db.Column(db.String(100), nullable=False)
 
     def __init__(self, nome, email, genero, ddi, ddd, telefone, senha):
