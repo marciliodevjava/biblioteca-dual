ae26b69f754f20ccaf3f2c2ad23c735c3164fa68
marciliodevjava <marciliodevjava@gmail.com>
2024-03-18 20:43:33 -0300

    Update usuario_resource

--- resource/usuario_resource.py
+++ resource/usuario_resource.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from requests import request
 
+from enumerate.message import UsuarioMessage
 from form.usuario_shema import UsuarioShema
 from service.usuario_service import UsuarioService
 
@@ -10,6 +11,9 @@ class UsuarioResource(Resource):
     def post(self):
         dados = UsuarioShema().load(request.json())
         usuario = UsuarioService.cadastrar_usuario(dados)
+        if usuario.get('messsage').__eq__(UsuarioMessage.USUARIO_JA_EXISTE.value):
+            return usuario, 200
+        return usuario, 201
 
     def get(self, id):
         pass
