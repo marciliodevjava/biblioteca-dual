834aaf0979ad2899b5998dbdf13dd75d293143d3
marciliodevjava <marciliodevjava@gmail.com>
2024-03-20 15:58:52 -0300

    Update usuario_message

--- enumerate/message.py
+++ enumerate/message.py
@@ -14,6 +14,8 @@ class ErroServidorMessage(Enum):
 
 
 class UsuarioMessage(Enum):
+    USUARIO_NAO_ENCONTRADO = 'Usuário não encontrado.'
+    USUARIO_BUSCADO_COM_SUCESSO = 'Usuário buscado com sucesso.'
     USUARIO_CADASTRO_COM_SUCESSO = 'Usuario cadastrado com sucesso.'
     USUARIO_JA_EXISTE = 'Usuario já existe na base de dados.'
 
