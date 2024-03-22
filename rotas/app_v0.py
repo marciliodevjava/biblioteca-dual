96ed1263f41e100f2545355fdcb56fa415750607
marciliodevjava <marciliodevjava@gmail.com>
2024-03-22 20:29:14 -0300

    Update loginresource

--- rotas/app_v0.py
+++ rotas/app_v0.py
@@ -1,6 +1,6 @@
 from flask_restful import Api
 
-from resource.usuario_resource import UsuarioResource
+from resource.usuario_resource import UsuarioResource, LoginResource
 from variaveis.variaveis_do_sistema import BASE_PATH_HTTP
 
 
@@ -10,4 +10,6 @@ def config_routes_v0(api: Api):
     api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro/<int:id>', methods=['PUT'], endpoint='atualizar_usuario')
     api.add_resource(UsuarioResource, f'{BASE_PATH_HTTP}/cadastro/<int:id>', methods=['DELETE'], endpoint='deletar_usuario')
 
+    api.add_resource(LoginResource, f'{BASE_PATH_HTTP}/login', methods=['GET'], endpoint='buscar_token')
+    api.add_resource(LoginResource, f'{BASE_PATH_HTTP}/login', methods=['POST'], endpoint='login')
     return api
